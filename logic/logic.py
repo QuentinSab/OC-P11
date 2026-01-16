04ca0f60f7e4a54b8a6198675cf10cc9dbb009d1
QuentinSab <quentinsab@hotmail.fr>
2026-01-16 13:35:42 +0100

    install coverage, format server.py, move loadClubs() and loadCompetitions() to logic.py

--- logic/logic.py
+++ logic/logic.py
@@ -1,3 +1,18 @@
+import json
+
+
+def loadClubs():
+    with open("clubs.json") as c:
+        listOfClubs = json.load(c)["clubs"]
+        return listOfClubs
+
+
+def loadCompetitions():
+    with open("competitions.json") as comps:
+        listOfCompetitions = json.load(comps)["competitions"]
+        return listOfCompetitions
+
+
 def get_club_by_email(email, clubs):
     for club in clubs:
         if club["email"] == email:
